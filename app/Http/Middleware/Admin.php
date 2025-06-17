1127488553195a117622f1f5ed70dcc087c6e4fb
tawbazf <tawbazehaf@gmail.com>
2025-06-17 12:27:35 +0100

    feat: add Admin middleware to enforce admin access control

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Admin
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->user()->is_admin) {
+            return response()->json(['error' => 'Admin access required'], 403);
+        }
+        return $next($request);
+    }
+}
