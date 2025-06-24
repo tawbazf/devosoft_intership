7a2f052cead0bf925b76a649eea0817cadad62c9
tawbazf <tawbazehaf@gmail.com>
2025-06-24 13:12:23 +0100

    feat: enhance video upload endpoint to save video metadata in the database

--- fastapi-video-service/app/main.py
+++ fastapi-video-service/app/main.py
@@ -22,3 +22,35 @@ DATABASE_URL = "postgresql://postgres:tawba@localhost:5432/video"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from main import SessionLocal, engine
+from app import models
+
+# Crée les tables
+models.Base.metadata.create_all(bind=engine)
+
+# Dépendance pour avoir une session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+from app import schemas, models
+from sqlalchemy.orm import Session
+
+@app.post("/upload/", response_model=schemas.VideoOut)
+async def upload_video(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    result = await process_video(file)
+    
+    video = models.Video(
+        id=result["video_id"],
+        title="Video auto",
+        manifest_url=result["manifest_url"],
+        hls_url=result["hls_url"]
+    )
+    db.add(video)
+    db.commit()
+    db.refresh(video)
+    return video
